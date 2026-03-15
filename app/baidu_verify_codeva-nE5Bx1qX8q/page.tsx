d42741916da22f1a807f3dbfef76a0bc80e2d727
Aimeishun Developer <dev@aimeishun.com>
2026-03-15 13:41:24 +0800

    Add Baidu verification page as Next.js route

---
+++ app/baidu_verify_codeva-nE5Bx1qX8q/page.tsx
@@ -0,0 +1,10 @@
+// Baidu verification page
+// This page outputs the verification code for Baidu Search Resource Platform
+
+export default function BaiduVerification() {
+  return (
+    <html>
+      <body>codeva-nE5Bx1qX8q</body>
+    </html>
+  );
+}
